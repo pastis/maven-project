2d13c5b1fb429252e5fdd67b94727ddb4f9b120d
Edward Huth <edward.huth@oracle.com>
2018-01-21 00:27:56 -0700

    Fixing JAVA issues

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,5 @@
 package com.example;
-
+package info.java;
 /**
  * This is a class.
  */
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   *
+   */
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
